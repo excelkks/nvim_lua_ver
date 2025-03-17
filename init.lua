b3ed8a2e9cbcd2f5b1a4b97a64bc231105ab4180
excelkks <excelkks@hotmail.com>
2025-03-18 00:02:25 +0800

    basic configuration

---
+++ init.lua
@@ -0,0 +1,7 @@
+require('options')
+require('keymaps')
+require('plugins')
+require('colorscheme')
+require('lsp')
+
+-- https://martinlwx.github.io/zh-cn/config-neovim-from-scratch/#%E8%87%AA%E5%8A%A8%E8%A1%A5%E5%85%A8%E6%8C%87%E5%8D%97
---
+++ lua/options.lua
@@ -0,0 +1,37 @@
+vim.opt.jumpoptions = 'stack'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = '80'
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.cindent = true
+vim.opt.scrolloff = 4
+vim.opt.timeout = false
+vim.opt.wrap = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = false
+-- vim.opt.showmode = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.shortmess:append('mrwc')
+-- vim.opt.lazyredraw = true
+vim.opt.laststatus = 3
+vim.opt.virtualedit = 'block'
+vim.opt.mouse = 'a'
+vim.opt.cmdheight = 2
+vim.opt.updatetime = 300
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.termguicolors= true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.writebackup = false
+vim.opt.signcolumn = "yes"
+
+
